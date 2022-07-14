02c3d608695a71cfd862ccedb08025ffd51f1181
Seyoung Shin <pipixpfjs@gmail.com>
2022-07-14 16:13:04 +0900

    1226번 수정

--- Difficulty 4/1226_세영.java
+++ Difficulty 4/1226_세영.java
@@ -6,7 +6,7 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class D4_1226 {
-	static int[][] arr;
+	static int arr[][], res;
 	static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
 	static boolean[][] visited;
 
@@ -16,18 +16,26 @@ public class D4_1226 {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		for (int t = 1; t <= 1; t++) {
+		for (int t = 1; t <= 10; t++) {
 			br.readLine();
 			arr = new int[16][16];
 			visited = new boolean[16][16];
+			int x = 0, y = 0;
+			res = 0;
+			
 			for (int i = 0; i < 16; i++) {
 				char[] num = br.readLine().toCharArray();
 				for (int j = 0; j < 16; j++) {
 					arr[i][j] = num[j]-'0';
+					if (arr[i][j] == 2) {
+						x = i;
+						y = j;
+					}
 				}
 			}
 			
-			int res = dfs(1, 1);
+			visited[x][y] = true;
+			dfs(x, y);
 			
 			sb.append("#").append(t).append(" ").append(res).append("\n");
 		}
@@ -39,22 +47,22 @@ public class D4_1226 {
 
 	}
 	
-	static int dfs(int x, int y) {
-		if (arr[x][y] == 3)
-			return 1;
+	static void dfs(int x, int y) {
+		if (arr[x][y] == 3) {
+			res = 1;
+			return;
+		}
 		
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			if (nx > 0 && nx < 15 && ny > 0 && ny < 15 && arr[nx][ny]==0 && visited[nx][ny]==false) {
+			if (nx > 0 && nx < 15 && ny > 0 && ny < 15 && arr[nx][ny]!=1 && visited[nx][ny]==false) {
 				visited[nx][ny] = true;
 				dfs(nx, ny);
 				visited[nx][ny] = false;
 			}
 		}
-		
-		return 0;
 	}
 
 }
