ec8929600a4d57bf571c5cdc2a52e3775d3f47b1
Yoola Choi <39353959+yoooooola@users.noreply.github.com>
2022-08-09 01:32:23 +0900

    Update 21.cpp

--- 22 Summer Algorithm Lecture/21.cpp
+++ 22 Summer Algorithm Lecture/21.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<int> kindMoney;
 int kind[8] = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 
-int calMoney(int money, int kind, int idx) {
-    if (money/kind > 0)
-        kindMoney.emplace_back(money/kind);
-    else
-        kindMoney.emplace_back(0);
-
-    return money%kind;
-}
-
 void cashBack(int money) {
-    int idx = 0;
-    while(money >= 0 && idx < 8) {
-        money = calMoney(money, kind[idx], idx);
-        idx++;
+    for (int idx=0; idx<8; money>=0) {
+    	cout << money/kind[idx] << " ";
+        money %= kind[idx++];
     }
-
-    for (int i=0; i<8; i++)
-        cout << kindMoney[i] << " ";
+    cout << endl;
 }
 
 int main(void) {
@@ -32,14 +18,9 @@ int main(void) {
 
     for (int t=1; t<=testCase; t++) {
         cin >> n;
-
-        // init
-        kindMoney.clear();
-
         // print answer
         cout << "#" << t << endl;
         cashBack(n);
-        cout << endl;
     }
 
     return 0;
